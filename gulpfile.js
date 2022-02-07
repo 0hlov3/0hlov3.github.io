6b92e2936a8c350809de4886a93815c43e2434be
0hlov3 <0hlov3@protonmail.com>
2022-02-07 19:49:48 +0100

    some changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,14 @@ var gulp = require('gulp');
 var csso = require('gulp-csso');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
 var plumber = require('gulp-plumber');
 var cp = require('child_process');
 var imagemin = require('gulp-imagemin');
 var browserSync = require('browser-sync');
 
-var jekyllCommand = (/^win/.test(process.platform)) ? 'jekyll.bat' : 'bundle';
-
+//var jekyllCommand = (/^win/.test(process.platform)) ? 'jekyll.bat' : 'bundle';
+var jekyllCommand = 'bundle'
 /*
  * Build the Jekyll Site
  * runs a child process in node that runs the jekyll commands
@@ -21,25 +21,27 @@ gulp.task('jekyll-build', function (done) {
 /*
  * Rebuild Jekyll & reload browserSync
  */
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+
+gulp.task('jekyll-rebuild',  gulp.parallel('jekyll-build'), function () {
 	browserSync.reload();
 });
 
 /*
  * Build the jekyll site and launch browser-sync
  */
-gulp.task('browser-sync', ['jekyll-build'], function() {
+
+gulp.task('browser-sync', gulp.parallel('jekyll-build'), function() {
 	browserSync({
 		server: {
 			baseDir: '_site'
 		}
 	});
 });
-
+ 
 /*
 * Compile and minify sass
 */
-gulp.task('sass', function() {
+gulp.task('sass', async function() {
   	gulp.src('src/styles/**/*.scss')
 		.pipe(plumber())
 		.pipe(sass())
@@ -85,4 +87,4 @@ gulp.task('watch', function() {
 	gulp.watch(['*html', '_includes/*html', '_layouts/*.html'], ['jekyll-rebuild']);
 });
 
-gulp.task('default', ['js', 'sass', 'fonts', 'browser-sync', 'watch']);
+gulp.task('default',  gulp.parallel('js', 'sass', 'fonts', 'browser-sync', 'watch'));
